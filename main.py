dc0bed05f3e0d1e8a12871ff3651af50fa1f0f30
student <student@sgugit.ru>
2024-02-20 12:53:31 +0700

    Initial commit // first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("Input your name:\n")
+group = input("Your group:\n")
+email = input("And now, input your email:\n")
+print("Hello, student  " + name + " from the " + group + " group. \nYour email is: " + email)
